14e2413fda3fc81ee8213b9bff5d16578569a1d4
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-17 09:08:27 +0530

    cBasics

---
+++ Basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a = 10;
+    printf("The a value is %d\n",a);
+    printf("%d The a value is ",a);
+    printf("\n Hello welcome to C");
+    float avg = 82.5;
+    printf("The total Avg is: %2f",avg);
+    return 0;
+}
+
+// Variables and DataTypes
+// datatype variable_name = value
+// =-> assigment Operator -> assigment operator assign a right side value to left side variable
+// float avg = 82.5
