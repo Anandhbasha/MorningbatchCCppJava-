f614285d3bda54bbed24a9683349b14c73b65626
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-18 09:08:25 +0530

    datatypes

--- Basic.c
+++ Basic.c
@@ -1,15 +1,52 @@
+// #include <stdio.h>
+// int main(){
+//     int a = 10;
+//     printf("The a value is %d\n",a);
+//     printf("%d The a value is ",a);
+//     printf("\n Hello welcome to C");
+//     float avg = 82.5;
+//     printf("The total Avg is: %2f",avg);
+//     return 0;
+// }
+
+// // Variables and DataTypes
+// // datatype variable_name = value
+// // =-> assigment Operator -> assigment operator assign a right side value to left side variable
+// // float avg = 82.5
+
+
 #include <stdio.h>
 int main(){
-    int a = 10;
-    printf("The a value is %d\n",a);
-    printf("%d The a value is ",a);
-    printf("\n Hello welcome to C");
-    float avg = 82.5;
-    printf("The total Avg is: %2f",avg);
+    // int
+    // float
+    // char
+    char letter = 'A';
+    // printf("The leter is %c",letter);
+    // double
+    double salary = 2500000.589;
+    // Modified data type
+    // short
+    short int smallValue = 100;
+    // long
+    long int population = 784575655;
+    // long long
+    long long int postivenumber= 446545345745454;
+    // unsigned int
+    unsigned int pass = 500;
+    //signed int
+    signed int temparature = -20;
+
+    // charcter Variations
+    signed char sc = -100;
+    // unsigned char uc = 100;
+    // printf("The employees salary is:%lf",salary);
+    // printf("The employees salary is:%hd",smallValue);
+    // printf("The employees salary is:%ld",population);
+    // printf("The employees salary is:%lld",postivenumber);
+    // printf("The employees salary is:%u",pass);
+    // printf("The employees salary is:%c",sc);
+    // printf("The employees salary is:%u",uc);
+    // char name [] = "Ajay";
+    
     return 0;
 }
-
-// Variables and DataTypes
-// datatype variable_name = value
-// =-> assigment Operator -> assigment operator assign a right side value to left side variable
-// float avg = 82.5
---
+++ operators.c
@@ -0,0 +1,24 @@
+// arithmetic
+// +,-,*,/,%
+// (),
+// BODMAS
+// Bracket
+// off
+// Division
+// Multiplication
+// Add
+// Sub
+// relational
+// >,<,>=,<=,!=
+
+// Logical
+// Assigment
+// increment
+// decrement
+
+
+#include <stdio.h>
+int main(){
+    printf("The value is Graterthen or Equal %d",(10>=10));
+    return 0;
+}
